7dccf8caddb8fea0a114659d1d0a3fcdd31eac86
Bafyn <nikitin2705@mail.ru>
2017-02-04 14:32:37 +0200

    Убрана таблица с объявлениями "локально"

--- views/site/index.php
+++ views/site/index.php
@@ -68,47 +68,7 @@ foreach ($items as $value) {
             </div>
         </div>
 
-        <div class="content-table">
-            <h3><?php echo $type == 'buy'?"Купить ":"Продать "; ?> биткоины онлайн</h3>
-            <div class="table-1">
-                <?php if(($type == 'buy' &&$count_buy > 0)||($type == 'sell' && $count_sell > 0)){ ?>
-                <table>
-                    <tr>
-                        <th>Рейтинг</th>
-                        <th>Продавец</th>
-                        <th>Способ оплаты</th>
-                        <th>Цена</th>
-                        <th>Ограничения</th>
-                        <th></th>
-                    </tr>
-                    <?php
-                    foreach ($adses as $ads):
-                        $username = User::getUsernameById($ads['user_id']);
-                        $currency = Currency::getStringName($ads['currency_id']);
-                        $symbol = Currency::getSymbol($ads['currency_id']);
-                        if($ads['type'] == ($type == 'buy'?2:1)){
-                        ?>
-                        <tr>
-                            <td><span class="color_100">100%</span></td> <!--  color_100,50,35?> -->
-                            <td>
-                                <a href="/user?sender_id=<?= $_SESSION['id_user'] ?>&receiver_log=<?= $username ?>" class="<?php echo (User::isOnline($ads['user_id']))?("stic_online"):("off_online"); ?> color_blue table-ads-user-prof"><?= $username ?>
-                                    <span class="modalStatus"><?php echo (User::isOnline($ads['user_id']))?("Online"):("Offline"); ?></span>
-                                </a>
-                            </td>
-                            <td>Банковские переводы внутри странны (EOSWC)</td>
-                            <!-- <td>Банковские переводы внутри странны (EOSWC)</td> -->
-                            <!-- <td><span class="price_color"><?= $ads['price'], ' ', $currency . ' / BTC' ?></span></td> -->
-                            <td><span class="price_color"><?= $ads['price'], ' ', $currency?></span></td>
-                            <!-- <td><?= $ads['max_amount']?> </td> -->
-                            <td>30 - 8000 EUR</td>
-                            <td class="seg-bue-table"><a href="/cabinet/info?ads=<?= $ads['id_advertisement'] ?>" class="table-btn-bue"><?= $ads['user_id'] == User::getUserIdFromSession() ? 'Купить' : Advertisement::getInvertStringType($ads['type']) ?></td>
-                        </tr>
-                    <?php }endforeach;?>
-                </table>
-                <?php } else {?> 
-               <div style="color:grey;">Извините, но предложений по <?= $type=="buy"?"покупке ":"продаже ";?>пока нет</div><?php }?>
-            </div>
-        </div>
+        
     <?php else: ?>
         <br/>
         <div class="informate-messege">
